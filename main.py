9128890276b8bc72c31b63dcdbfc68d6b6783cbc
Makszim Weinrauch <makszim.weinrauch@active-agent.com>
2022-06-23 14:19:51 +0200

    Added basic code to bring LED to blink

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
-print("Hello World!")
+# https://themachineshop.uk/getting-started-with-the-pi-pico-and-pycharm/
+from machine import Pin
+import time
+
+led = Pin(25, Pin.OUT)
+
+while True:
+    led(1)
+    time.sleep(1)
+    led(0)
+    time.sleep(1)
